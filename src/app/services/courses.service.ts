f0650c20a2fe8124a99d0dcf804a28dcafa83ff8
CNLZgft <cristian.lopez@gft.com>
2023-09-07 17:12:30 +0200

    Courses service created as observable in order to make the http requests to the backend wherever it's needed Home component now is only calling the service to get the data from the backend Courses-card-list is a new presentational component to display the data and is displayed in home component passing the attribute "courses" as Input to pass the different observables (advance and begginer)

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map, shareReplay } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((response) => response["payload"]),
+      //para evitar múltiples llamadas al backend para devolver los mismos datos se usa el "shareReplay"
+      shareReplay()
+    );
+  }
+}
