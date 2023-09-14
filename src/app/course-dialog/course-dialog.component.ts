ccc0c10368291b1017c2781b39d7954271839757
CNLZgft <cristian.lopez@gft.com>
2023-09-14 10:51:12 +0200

    catchError no es necesario porque se hará en el CourseStore, se ha definido la API en CourseStore para guardar el formulario, courseDialog component ahora consume la API de guardao del COurseStore mediante la subscripción

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -16,6 +16,7 @@ import { throwError } from "rxjs";
 import { CoursesService } from "../services/courses.service";
 import { LoadingService } from "../loading/loading.service";
 import { MessagesService } from "../messages/messages.service";
+import { CourseStore } from "../services/courses.store";
 
 @Component({
   selector: "course-dialog",
@@ -32,9 +33,10 @@ export class CourseDialogComponent implements AfterViewInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
-    private messagesService: MessagesService
+    //private coursesService: CoursesService,
+    //private loadingService: LoadingService,
+    private messagesService: MessagesService,
+    private coursesStore: CourseStore
   ) {
     this.course = course;
 
@@ -51,22 +53,30 @@ export class CourseDialogComponent implements AfterViewInit {
   save() {
     const changes = this.form.value;
 
-    const saveCourse$ = this.coursesService
-      .saveCourse(this.course.id, changes)
-      .pipe(
-        catchError((err) => {
-          const message = "Could not save the course";
-          this.messagesService.showErrors(message);
-          console.log(message, err);
-          return throwError(err);
-        })
-      );
+    //MODO STATELESS
+    //en el modo STATEFULL se encarga el CourseStore de hacer el loading,
+    //así que en este caso no hace falta
+    // const saveCourse$ = this.coursesStore
+    //   .saveCourse(this.course.id, changes)
+    //   .pipe(
+    //     catchError((err) => {
+    //       const message = "Could not save the course";
+    //       this.messagesService.showErrors(message);
+    //       console.log(message, err);
+    //       return throwError(err);
+    //     })
+    //   );
 
-    this.loadingService
-      .showLoaderUntilCompleted(saveCourse$)
-      .subscribe((value) => {
-        this.dialogRef.close(value);
-      });
+    // this.loadingService
+    //   .showLoaderUntilCompleted(saveCourse$)
+    //   .subscribe((value) => {
+    //     this.dialogRef.close(value);
+    //   });
+
+    //MODO STATEFULL
+    this.coursesStore.saveCourse(this.course.id, changes).subscribe();
+
+    this.dialogRef.close(changes);
   }
 
   close() {
