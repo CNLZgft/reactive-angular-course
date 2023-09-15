51cb9bfb4f34af70d1be52bf442537b097657cc7
CNLZgft <cristian.lopez@gft.com>
2023-09-15 15:51:41 +0200

    para que se emitan dos observables de una sola vez, y no se necesite repetir tanto código

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,54 +1,73 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
 import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
-import {Lesson} from '../model/lesson';
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from "rxjs";
+import { Lesson } from "../model/lesson";
+import { CoursesService } from "../services/courses.service";
+import { map, tap } from "rxjs/operators";
 
-
-@Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
-})
-export class CourseComponent implements OnInit {
-
+interface CourseData {
   course: Course;
-
   lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
-  }
-
-  ngOnInit() {
-
-
-
-  }
-
-
 }
 
+@Component({
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
+})
+export class CourseComponent implements OnInit {
+  //usando un observable
+  data$: Observable<CourseData>;
 
+  //usando múltiples observables para  visualizar los datos de
+  //los cursos y las lecciones
+  // course$: Observable<Course>;
 
+  // lessons$: Observable<Lesson[]>;
 
+  // lessons: Lesson[];
 
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) {}
 
+  ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
+    //múltiples observables
+    // this.course$ = this.coursesService.loadCourseById(courseId);
 
+    // this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
 
+    //usando un observable, con el combineLatest
+    const course$ = this.coursesService.loadCourseById(courseId);
 
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
 
+    //la posicion 0 emitira valores de course$ y la posicion 1 emitira valores de lessons$
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
+  }
+}
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -38,4 +38,24 @@ export class CoursesService {
         shareReplay()
       );
   }
+
+  loadCourseById(courseId: number) {
+    return this.http
+      .get<Course>(`/api/courses/${courseId}`)
+      .pipe(shareReplay());
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          courseId: courseId.toString(),
+          pageSize: "10000",
+        },
+      })
+      .pipe(
+        map((response) => response["payload"]),
+        shareReplay()
+      );
+  }
 }
